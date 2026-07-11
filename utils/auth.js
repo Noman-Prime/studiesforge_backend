f85a9995ebfb1ccd57f42aa343989b0d548d2a87
Noman-Prime <your-email@example.com>
2026-07-11 23:40:23 +0500

    Login token issue is fixed

--- utils/auth.js
+++ utils/auth.js
@@ -3,7 +3,7 @@ import User from "../models/user.js";
 
 export const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies?.token;
+        const {token} = req.cookies;
 
         if (!token) {
             return res.status(401).json({
