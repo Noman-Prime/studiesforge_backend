c11ab8f4e8e267024601480f85d4e8541d39eb07
Noman-Prime <your-email@example.com>
2026-07-12 00:22:17 +0500

    Login token issue is fixed

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,12 +1,13 @@
-export const sendToken = (user, statusCode, res) =>{
-        const token = user.jsonWebToken()
-        const options = {
-            expires: new Date(Date.now() + 7* 24 * 60 * 60 * 1000),
-            httpOnly : true
-        }
-        return res.status(statusCode).cookie("token", token, options).json({
-            success: true,
-            user,
-            token
-        })
+export const sendToken = (user, statusCode, res) => {
+    const token = user.jsonWebToken()
+    const options = {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true,
+    }
+    return res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        user,
+        token
+    })
 }
