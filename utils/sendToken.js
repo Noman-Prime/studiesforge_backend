a6e88cb9b4f0d4f5e1c514428bd5de6926ed0fd8
Noman-Prime <your-email@example.com>
2026-07-12 00:42:47 +0500

    token file updated

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,6 +4,8 @@ export const sendToken = (user, statusCode, res) => {
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: true,
+        sameSite: "none",
+        path: "/",
     }
     return res.status(statusCode).cookie("token", token, options).json({
         success: true,
